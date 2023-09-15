323cb46241edf56fbeb31559a9ea25f630131d4d
Stephan Kern <stephan@swmmrman.com>
2023-09-15 09:59:35 -0600

    Make index and hello "pages"

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello world."
+    "Home page goes here."
+}
+
+#[get("/hello")]
+fn hello() -> &'static str {
+    "Hello world"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index,hello])
 }
